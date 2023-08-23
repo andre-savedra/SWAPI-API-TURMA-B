66588c0e2da6f6e5f8ac8b305a4c4d66c6b968b2
andre-savedra <andresavedracruz@gmail.com>
2023-08-23 18:17:56 -0300

    aula 22-08-23

--- main/views.py
+++ main/views.py
@@ -12,10 +12,27 @@ class PeopleAPIView(APIView):
             serializer = PeopleSerializer(peopleFound, many=True) #JSON
             return Response(serializer.data) #devolvendo ao cliente o JSON!
         #busca por people id:
-        peopleFound = People.objects.get(id=peopleId) #select *from people where id = peopleId 
-        serializer = PeopleSerializer(peopleFound, many=False) #JSON
-        return Response(serializer.data)
-        
+        try:
+            peopleFound = People.objects.get(id=peopleId) #select *from people where id = peopleId 
+            serializer = PeopleSerializer(peopleFound, many=False) #JSON
+            return Response(serializer.data)
+        except People.DoesNotExist:
+            return Response(status=404,  data="People not Found!!!!")
+    
+    def post(self, request):
+        #pega o Json do cliente e guardando na variavel
+        peopleJson = request.data
+        #convertendo o JSON em Python!!!!
+        peopleSerialized = PeopleSerializer(data=peopleJson, many=False) 
+        #verificar se os dados estão coerentes!!!!
+        if peopleSerialized.is_valid():
+            #salvando no banco de dados!!!!
+            peopleSerialized.save()
+            return Response(status=203, data=peopleSerialized.data)
+        return Response(status=400, data="Mande certo seu imbecil!")
+
+
+
 class PlanetAPIView(APIView):
     def get(self, request):
         planetFound = Planet.objects.all() #select *from planet; #PYTHON
