615c73b6698c6878d9103bb2ebdd340f01a27ff0
andre-savedra <andresavedracruz@gmail.com>
2023-08-29 21:42:30 -0300

    test

--- main/views.py
+++ main/views.py
@@ -2,22 +2,43 @@ from .models import *
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django_filters import rest_framework as filters
+# class PeopleFilters(DjangoFilterBackend):
+#     def filter_queryset(self, request, queryset, view):
+#         filter_class = self.get_filter_class(view, queryset)
+
+#         if filter_class:
+#             return filter_class(request.query_params, queryset=queryset, request=request).qs
+#         return queryset
 
 class PeopleAPIView(APIView):
-    def get(self, request, peopleId = ''):
-        #se o get não tiver o filtro de id:
-        if peopleId == '':   
-            #seleciona tudo!     
-            peopleFound = People.objects.all() #select *from people; #PYTHON
-            serializer = PeopleSerializer(peopleFound, many=True) #JSON
-            return Response(status=200, data=serializer.data) #devolvendo ao cliente o JSON!
-        #busca por people id:
-        try:
-            peopleFound = People.objects.get(id=peopleId) #select *from people where id = peopleId 
-            serializer = PeopleSerializer(peopleFound, many=False) #JSON
-            return Response(status=200,data=serializer.data)
-        except People.DoesNotExist:
-            return Response(status=404,  data="People not Found!!!!")
+    # serializer_class = PeopleSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('name', 'eyeColor')
+    def filter_queryset(self, queryset):
+        for backend in list(self.filter_backends):
+            queryset = backend().filter_queryset(self.request, queryset, self)
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        peopleFound = People.objects.all()
+        filtered_qs = self.filter_queryset(peopleFound)
+        serialized = PeopleSerializer(filtered_qs, many=True)
+        return Response(serialized.data)
+        
+        # #se o get não tiver o filtro de id:
+        # if peopleId == '':   
+        #     #seleciona tudo!     
+        #     peopleFound = People.objects.all() #select *from people; #PYTHON
+        #     serializer = PeopleSerializer(peopleFound, many=True) #JSON
+        #     return Response(status=200, data=serializer.data) #devolvendo ao cliente o JSON!
+        # #busca por people id:
+        # try:
+        #     peopleFound = People.objects.get(id=peopleId) #select *from people where id = peopleId 
+        #     serializer = PeopleSerializer(peopleFound, many=False) #JSON
+        #     return Response(status=200,data=serializer.data)
+        # except People.DoesNotExist:
+        #     return Response(status=404,  data="People not Found!!!!")
     
     def post(self, request):
         #pega o Json do cliente e guardando na variavel
