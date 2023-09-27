84f15082392e1f6aab957fc2499dd6a59469d08e
andre-savedra <andresavedracruz@gmail.com>
2023-09-26 21:26:33 -0300

    adicionado filtros customizados

--- main/views.py
+++ main/views.py
@@ -3,13 +3,26 @@ from .serializers import *
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+import django_filters
+
+
+#crio um filter personalizado para o People fazendo com que busque os nomes com o LIKE no banco de dados
+# e também as alturas que sejam maiores ou iguais ao que especificar
+class PeopleFilter(django_filters.FilterSet):
+     name = django_filters.CharFilter(lookup_expr='icontains')
+     height = django_filters.NumberFilter(lookup_expr='gte')
+
+     class Meta:
+          model = People
+          fields = ['name', 'height']
 
 
 class PeopleAPIView(ModelViewSet):
      queryset = People.objects.all() #informa p/ a lib qual as consultas a serem feitas
      serializer_class = PeopleSerializer #informa o serializer
      filter_backends = [DjangoFilterBackend] #usa a lib django-filter
-     filterset_fields = ['name', 'eyeColor', 'height', 'gender']
+     filterset_class = PeopleFilter #registra o filtro customizado
+
         
 class PlanetAPIView(ModelViewSet):
      queryset = Planet.objects.all() #informa p/ a lib qual as consultas a serem feitas
